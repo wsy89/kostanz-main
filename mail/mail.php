5b63dfe0e629998d86da182beff72ff1e067a5c0
Leanne Won <wonleanne@gmail.com>
2018-05-19 21:56:22 +1200

    update app password

--- mail/mail.php
+++ mail/mail.php
@@ -16,8 +16,8 @@ $outgoingServerSecurity     = 'tls';                 // Options: 'ssl' , 'tls' ,
 
 // Sending Account Settings - replace these details with an email account held on the SMTP server entered above.
 // These 2 settings are only required if you choose 'smtp' for emailMethod above.
-$sendingAccountUsername     = 'kostanz@gmail.com';
-$sendingAccountPassword     = '1peter211';
+$sendingAccountUsername     = 'media.kostanz@gmail.com';
+$sendingAccountPassword     = 'oppojgtqtpilootf';
 
 // Recipient (To:) Details  - Change this to the email details of who will receive all the emails from the website.
 $recipientEmail             = 'kostanz@gmail.com'; // REQUIRED value.
