22899d20034fe805f46ce39370b3068efcf9d3ce
Nick Ko <sy.nick.ko019@gmail.com>
2018-05-19 21:38:49 +1200

    setup smtp

--- mail/mail.php
+++ mail/mail.php
@@ -6,12 +6,12 @@
 
 // Choose here whether to use php mail() function or your SMTP server (recommended) to send the email.
 // Use 'smtp' for better reliability, or use 'phpmail' for quick + easy setup with lower reliability.
-$emailMethod                = 'phpmail'; // REQUIRED value. Options: 'smtp' , 'phpmail'
+$emailMethod                = 'smtp'; // REQUIRED value. Options: 'smtp' , 'phpmail'
 
 // Outgoing Server Settings - replace values on the right of the = sign with your own.
 // These 3 settings are only required if you choose 'smtp' for emailMethod above.
-$outgoingServerAddress      = 'ssl://smtp.gmail.com'; // Consult your hosting provider.
-$outgoingServerPort         = '465';                  // Options: '587' , '25' - Consult your hosting provider.
+$outgoingServerAddress      = 'smtp.gmail.com'; // Consult your hosting provider.
+$outgoingServerPort         = '587';                  // Options: '587' , '25' - Consult your hosting provider.
 $outgoingServerSecurity     = 'tls';                 // Options: 'ssl' , 'tls' , null - Consult your hosting provider.
 
 // Sending Account Settings - replace these details with an email account held on the SMTP server entered above.
@@ -27,7 +27,7 @@ $recipientName              = 'KOSTA New Zealand';             // REQUIRED value
 $emailSubject               = 'A message from Your Website'; // REQUIRED value. Subject of the email that the recipient will see
 $websiteName                = 'KOSTA NZ';                // REQUIRED value. This is used when a name or email is not collected from the website form
 
-$timeZone					= 'New Zealand/Auckland';         // OPTIONAL, but some servers require this to be set.
+$timeZone					= '';         // OPTIONAL, but some servers require this to be set.
                                                              // See a list of all supported timezones at: http://php.net/manual/en/timezones.php
  /*----------------------------------------------------------------------------*\
 |*  You do not need to edit anything below this line, the rest is automatic.    *|
